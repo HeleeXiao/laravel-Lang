8770360f05fdc1a936ecc446df450dd3fc699ed1
xiaochao <18681032630@163.com>
2017-05-05 12:06:56 +0800

    mirateions

--- database/migrations/2017_05_02_164045_create_lang_table.php
+++ database/migrations/2017_05_02_164045_create_lang_table.php
@@ -19,10 +19,10 @@ class CreateLangTable extends Migration
                 $table->integer('keyword_id')->comment("关联的语言词汇");
                 $table->integer('file_id')->comment("关联的附件");
                 $table->string('title');
-                $table->text('description');
+                $table->text('description')->nullable();
                 $table->tinyInteger('person')->comment("责任人")->default('1');
                 $table->tinyInteger('sponsor')->comment("发起人")->default('4');
-                $table->string('comment')->comment("修改说明");
+                $table->string('comment')->comment("修改说明")->nullable();
                 $table->integer('status')->comment("状态：0正常，1待处理，2已处理，3弃用")->nullable()->default(0);
                 $table->integer('type')->comment("类型：1后台，0前台")->nullable()->default(1);
                 $table->timestamps();
